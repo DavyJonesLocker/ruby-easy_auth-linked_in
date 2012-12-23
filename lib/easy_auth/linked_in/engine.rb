a53e9a7f33d543756af4294449c9105c96ef9bf5
Brian Cardarella <bcardarella@gmail.com>
2012-12-23 12:48:13 -0500

    Remove test framework opinion

--- lib/easy_auth/linked_in/engine.rb
+++ lib/easy_auth/linked_in/engine.rb
@@ -1,9 +1,5 @@
 module EasyAuth::LinkedIn
   class Engine < ::Rails::Engine
     isolate_namespace EasyAuth::LinkedIn
-
-    config.generators do |g|
-      g.test_framework :rspec, :view_specs => false
-    end
   end
 end
